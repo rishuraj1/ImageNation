04b5315c636bab93ce5dd7dc088e9a952504af29
Rishu <rrishu561@gmail.com>
2023-05-16 23:06:42 +0530

    add: input feature & initialized backend

--- script.js
+++ script.js
@@ -37,3 +37,45 @@ function openSignIn() {
 function openSignUp() {
     window.location.href = "signup.html";
 }
+
+
+// Open AI Section
+
+
+
+
+const dalleEndPoint = "https://api.openai.com/v1/images/generations"
+const reqBtn = document.getElementById("send-request");
+
+reqBtn.onclick = function () {
+    const API_KEY = document.getElementById("api-key").value;
+
+    // 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
+
+    const count = Number(document.getElementById("image-count").value);
+
+    const prompt = document.getElementById("image-prompt").value;
+
+    const reqBody = {
+        prompt: prompt,
+        n: count,
+        size: '1024x1024',
+        response_format: 'b64_json'
+    }
+    // console.log(reqBody);
+
+    const reqParams = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${API_KEY}`,
+        },
+        body: JSON.stringify(reqBody)
+    }
+
+    fetch(dalleEndPoint, reqParams)
+        .then(res => res.json())
+        .then(json => console.log(json));
+}
+
+
